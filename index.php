250c45a52db07c9d09f3e9912d3cd88a86561279
Suraj komarla <kasuraj98@gmail.com>
2022-10-06 14:19:26 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<?php include ( "inc/connect.inc.php" ); ?>
+<?php 
+ob_start();
+session_start();
+if (!isset($_SESSION['user_login'])) {
+	$user = "";
+}
+else {
+	$user = $_SESSION['user_login'];
+	$result = mysql_query("SELECT * FROM user WHERE id='$user'");
+		$get_user_email = mysql_fetch_assoc($result);
+			$uname_db = $get_user_email['firstName'];
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Welcome to ebuy online shop</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<script src="/js/homeslideshow.js"></script>
+		<script>
+		function showResult(str) {
+		  if (str.length==0) {
+		    document.getElementById("newsearch").innerHTML="";
+		    document.getElementById("newsearch").style.border="0px";
+		    return;
+		  }
+		  if (window.XMLHttpRequest) {
+		    // code for IE7+, Firefox, Chrome, Opera, Safari
+		    xmlhttp=new XMLHttpRequest();
+		  } else {  // code for IE6, IE5
+		    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		  }
+		  xmlhttp.onreadystatechange=function() {
+		    if (this.readyState==4 && this.status==200) {
+		      document.getElementById("newsearch").innerHTML=this.responseText;
+		      document.getElementById("newsearch").style.border="1px solid #A5ACB2";
+		    }
+		  }
+		  xmlhttp.open("GET","livesearch.php?q="+str,true);
+		  xmlhttp.send();
+		}
+		</script>
+	</head>
+	<body style="min-width: 980px;">
+		<div class="homepageheader" style="position: relative;">
+			<div class="signinButton loginButton">
+				<div class="uiloginbutton signinButton loginButton" style="margin-right: 40px;">
+					<?php 
+						if ($user!="") {
+							echo '<a style="text-decoration: none; color: #fff;" href="logout.php">LOG OUT</a>';
+						}
+						else {
+							echo '<a style="color: #fff; text-decoration: none;" href="signin.php">SIGN IN</a>';
+						}
+					 ?>
+					
+				</div>
+				<div class="uiloginbutton signinButton loginButton" style="">
+					<?php 
+						if ($user!="") {
+							echo '<a style="text-decoration: none; color: #fff;" href="profile.php?uid='.$user.'">Hi '.$uname_db.'</a>';
+						}
+						else {
+							echo '<a style="text-decoration: none; color: #fff;" href="login.php">LOG IN</a>';
+						}
+					 ?>
+				</div>
+			</div>
+			<div style="float: left; margin: 5px 0px 0px 23px;">
+				<a href="index.php">
+					<img style=" height: 75px; width: 130px;" src="image/ebuybdlogo.png">
+				</a>
+			</div>
+			<div class="">
+				<div id="srcheader">
+					<form id="newsearch" method="get" action="search.php">
+					        <input type="text" class="srctextinput" name="keywords" size="21" maxlength="120" onkeyup="showResult(this.value)" placeholder="Search Here..."><input type="submit" value="search" class="srcbutton" >
+					</form>
+				<div class="srcclear"></div>
+				</div>
+			</div>
+		</div>
+		<div class="home-welcome">
+			<div class="home-welcome-text" style="background-image: url(image/homebackgrndimg1.png); height: 380px; ">
+				<h1 style="margin: 0px;">Welcome To eBuy</h1>
+				<h2>Largest Online Shopping In Bangalore</h2>
+			</div>
+		</div>
+		<div class="home-prodlist">
+			<div>
+				<h3 style="text-align: center;">Products Category</h3>
+			</div>
+			<div style="padding: 20px 30px; width: 85%; margin: 0 auto;">
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="grocery/vegetables.php">
+							<img src="./image/product/vegetables/product-1.jpg" class="home-prodlist-imgi">
+							</a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="grocery/fruits.php">
+							<img src="./image/product/fruits/category-2.jpg" class="home-prodlist-imgi">
+							</a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="grocery/household.php">
+							<img src="./image/product/household/1.png" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="grocery/bakery.php">
+							<img src="./image/product/bakery/39.png" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="grocery/grains.php">
+							<img src="./image/product/grains/wheat.jpg" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="grocery/pulses.php">
+							<img src="./image/product/pulses/tur.jpg" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="grocery/diary.php">
+							<img src="./image/product/diary/eggs.jpg" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="grocery/cereal.php">
+							<img src="./image/product/diary/cheese.jpg" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+			</div>			
+		</div>
+	</body>
+</html>
---
+++ signin.php
@@ -0,0 +1,156 @@
+<?php include ( "inc/connect.inc.php" ); ?>
+<?php
+ob_start();
+session_start();
+if (!isset($_SESSION['user_login'])) {
+}
+else {
+	header("location: index.php");
+}
+
+$u_fname = "";
+$u_lname = "";
+$u_email = "";
+$u_mobile = "";
+$u_address = "";
+$u_pass = "";
+
+if (isset($_POST['signup'])) {
+//declere veriable
+$u_fname = $_POST['first_name'];
+$u_lname = $_POST['last_name'];
+$u_email = $_POST['email'];
+$u_mobile = $_POST['mobile'];
+$u_address = $_POST['signupaddress'];
+$u_pass = $_POST['password'];
+$confirmCode   = substr( rand() * 900000 + 100000, 0, 6 );
+//triming name
+$_POST['first_name'] = trim($_POST['first_name']);
+$_POST['last_name'] = trim($_POST['last_name']);
+	
+		// Check if email already exists
+		
+        if (mysql_query("SELECT firstname FROM user WHERE firstname='$u_fname'")) {
+                if (strlen($u_fname) >= 3 && strlen($u_fname) <= 32) {
+                        if (preg_match('/[a-zA-Z0-9_]+/', $u_fname)) {
+                                if (strlen($u_pass) >= 6 && strlen($u_pass) <= 60) {
+                                if (filter_var($u_email, FILTER_VALIDATE_EMAIL)) {
+                                if (mysql_query("SELECT email FROM user WHERE email='$u_email'")) {
+                                        mysql_query("INSERT INTO user (firstName,lastName,email,mobile,address,password,confirmcode,activation) VALUES ('$_POST[first_name]','$_POST[last_name]','$_POST[email]','$_POST[mobile]','$_POST[signupaddress]','$_POST[password]','$confirmCode','yes')");
+                                        echo "Success!";
+                                } else {
+                                        echo 'Email in use!';
+                                }
+                        } else {
+                                        echo 'Invalid email!';
+                                }
+                        } else {
+                                echo 'Invalid password!';
+                        }
+                        } else {
+                                echo 'Invalid username';
+                        }
+                } else {
+                        echo 'Invalid username';
+                }
+        } else {
+                echo 'User already exists!';
+        }
+}
+	
+
+?>
+
+
+<!doctype html>
+<html>
+	<head>
+		<title>Welcome to ebuybd online shop</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body class="home-welcome-text" style="background-image: url(image/homebackgrndimg2.png);">
+		<div class="homepageheader" style="position: inherit;">
+			<div class="signinButton loginButton">
+				<div class="uiloginbutton signinButton loginButton" style="margin-right: 40px;">
+					<a style="text-decoration: none;" href="signin.php">SIGN IN</a>
+				</div>
+				<div class="uiloginbutton signinButton loginButton" style="">
+					<a style="text-decoration: none;" href="login.php">LOG IN</a>
+				</div>
+			</div>
+			<div style="float: left; margin: 5px 0px 0px 23px;">
+				<a href="index.php">
+					<img style=" height: 75px; width: 130px;" src="image/ebuybdlogo.png">
+				</a>
+			</div>
+			<div class="">
+				<div id="srcheader">
+					<form id="newsearch" method="get" action="http://www.google.com">
+					        <input type="text" class="srctextinput" name="q" size="21" maxlength="120"  placeholder="Search Here..."><input type="submit" value="search" class="srcbutton" >
+					</form>
+				<div class="srcclear"></div>
+				</div>
+			</div>
+		</div>
+		<?php 
+			if(isset($success_message)) {echo $success_message;}
+			else {
+				echo '
+					<div class="holecontainer" style="float: right; margin-right: 36%; padding-top: 26px;">
+						<div class="container">
+							<div>
+								<div>
+									<div class="signupform_content">
+										<h2>Sign Up Form!</h2>
+										<div class="signupform_text"></div>
+										<div>
+											<form action="" method="POST" class="registration">
+												<div class="signup_form">
+													<div>
+														<td >
+															<input name="first_name" id="first_name" placeholder="First Name" required="required" class="first_name signupbox" type="text" size="30" value="'.$u_fname.'" >
+														</td>
+													</div>
+													<div>
+														<td >
+															<input name="last_name" id="last_name" placeholder="Last Name" required="required" class="last_name signupbox" type="text" size="30" value="'.$u_lname.'" >
+														</td>
+													</div>
+													<div>
+														<td>
+															<input name="email" placeholder="Enter Your Email" required="required" class="email signupbox" type="email" size="30" value="'.$u_email.'">
+														</td
+			>										</div>
+													<div>
+														<td>
+															<input name="mobile" placeholder="Enter Your Mobile" required="required" class="email signupbox" type="text" size="30" value="'.$u_mobile.'">
+														</td>
+													</div>
+													<div>
+														<td>
+															<input name="signupaddress" placeholder="Write Your Full Address" required="required" class="email signupbox" type="text" size="30" value="'.$u_address.'">
+														</td>
+													</div>
+													<div>
+														<td>
+															<input name="password" id="password-1" required="required"  placeholder="Enter New Password" class="password signupbox " type="password" size="30" value="'.$u_pass.'">
+														</td>
+													</div>
+													<div>
+														<input name="signup" class="uisignupbutton signupbutton" type="submit" value="Sign Me Up!">
+													</div>
+												</div>
+											</form>
+											
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				';
+			}
+
+		 ?>
+	</body>
+</html>
